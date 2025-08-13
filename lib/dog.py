194afcac87925994259a209fa49df2e43b921e59
AMOS KIPKORIR <amos.kipkorir@student.moringaschool.com>
2025-08-13 14:33:25 +0300

    updates all the latest commits

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,28 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
+    def __init__(self, name = "Rex", breed = "Mastiff"):
+        self.breed = breed
+        self.name = name
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:   
+            print("Name must be string between 1 and 25 characters.")    
+
+        pass
+    @property
+    def breed(self):
+        return self._breed
+    @breed.setter
+    def breed(self, value):
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+            
     pass
